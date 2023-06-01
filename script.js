bf8045532f7c2afcde7e6976b3fb71005dcc2920
Taniya <tanya.kalra200@gmail.com>
2023-06-01 19:21:01 +0530

    project created

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ function navigateToVideo(videoId){
  // video_id: video_id
     document.cookie = `video_id=${videoId}; path=${path}`
     let linkItem = document.createElement("a");
-    linkItem.href = "http://127.0.0.1:5500/video.html"
+    linkItem.href = "/video.html"
     linkItem.target = "_blank" ;
     linkItem.click();
   }
